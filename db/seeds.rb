a0336cfe44bc42dd3fa935a482bd2e0f706b7e0f
Pheebes <phoebe.hardwicke@waterfield.com>
2015-11-30 15:52:09 -0600

    Depot Scaffold

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.delete_all
+Product.create!(title: "Programming Ruby 1.9 & 2.0",
+        description: %{<p>Ruby is amazing etc</p>},
+        image_url: "ruby.png",
+        price: 49.95)
