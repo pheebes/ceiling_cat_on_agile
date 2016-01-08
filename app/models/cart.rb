8d1d664001af1f090922f5e105dc4c1f1606ae80
Pheebes <phoebe.hardwicke@waterfield.com>
2016-01-08 15:43:41 -0600

    Currently trying to fix several bugs.  One is the invalid cart message doesn't show up and the other is I don't think line items are being associated to a cart on creation.
    Page 126

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,12 @@
 class Cart < ActiveRecord::Base
 	has_many :line_items, dependent: :destroy
+
+	def add_product(product_id)
+		current_item = line_items.where(product_id: product_id).first_or_initialize
+		if current_item
+		  current_item.quantity += 1
+		  current_item.save
+		  current_item
+		end
+	end
 end
